09a35aa3036e70b54d34939f5d4d38d379fdd0bb
Eiko Ujifusa <eikorouji@gmail.com>
2022-03-19 17:00:11 -0700

    changed my vs code text editor font to source code pro to get rid of identical i and l mistakes

--- assets/js/weather-check.js
+++ assets/js/weather-check.js
@@ -1,2 +1,5 @@
 // my api key for this project: 8f9e52c6b60e9de371fda1c4748cde23 //
 
+// call for current weather // 
+
+// search for city, weather gets displayed to current-weather-info section in html //
