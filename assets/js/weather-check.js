a6601426557237c7c68c8e3279d629363334f1d5
Eiko Ujifusa <eikorouji@gmail.com>
2022-03-20 19:19:06 -0700

    trying to do the first fetch to OpenWeather API, using my api key.

--- assets/js/weather-check.js
+++ assets/js/weather-check.js
@@ -1,38 +1,16 @@
 // my api key for this project: f95d4558f1f3deec5b9d71145b7381a7 //
 
+// module example: 
+// fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+// response.json().then(function(data) {
+//    console.log(data);
+//  });
+//  });
 
 
-var getCurrentWeather = document.querySelector(".city-search-form");
-
-var searchCityName = document.querySelector(".searched-city-name");
-
-var searchInputButton = document.querySelector(".search-city-form-input");
-
-
-
-var getCurrentWeather = function(event) {
-    // open weather APIurl //
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
-    
-    var apiKey = "f95d4558f1f3deec5b9d71145b7381a7";
-
-
-    // make request to ow API //
-    fetch(apiUrl)
-    .then(function(response) {
-        // request was successful //
-        if (response.ok) {
-            console.log(response);
-            response.json().then(function(data) {
-                console.log(data);
-                displayRepos(data, user);
-            });
-        } else {
-            alert("Error:" + response.statusText);
-        }
-    })
-        .catch(function(error) {
-            alert("City not found! Please enter a city name.");
-        });
-    };
-
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={f95d4558f1f3deec5b9d71145b7381a7}")
+  .then(function(response) {
+ response.json().then(function(data) {
+     console.log(data);
+ });
+});
