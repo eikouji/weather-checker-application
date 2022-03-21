4850dc07f89ec096a78aa753421c3e21ced3ad40
Eiko Ujifusa <eikorouji@gmail.com>
2022-03-21 09:06:37 -0700

    added a .env to mask api key

--- assets/js/weather-check.js
+++ assets/js/weather-check.js
@@ -1,7 +1,39 @@
 // my api key for this project: f95d4558f1f3deec5b9d71145b7381a7 //
 
-// call for current weather // 
+// vars for this project //
+var searchedCityForm = document.querySelector("#searched-city-form");
+var searchedCityNameInput = document.querySelector("#search-city-form-input");
+var currentWeatherCurrentCity = document.querySelector("#current-city-name");
 
+require('dotenv').config()
+
+// call for current weather // 
+var getCurrentWeatherData = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city "&appid=f95d4558f1f3deec5b9d71145b7381a7&units=imperial";
+    fetch(apiUrl)
+      .then(function(response) {
+          if (response.ok) {
+              response.json().then(function(data){
+                  saveHistory.push(city);
+                  localStorage.setItem("cityName", JSON.stringify(saveHistory));
+                  displaySearchedCity(data, city);
+
+                  // latitude 
+
+                  // longitude
+
+                  // name 
+
+                  // OW oneCall API // 
+
+              })
+          }
+      })
+}
+
+var displaySearchedCity = function(weatherData, citySearch) {
+    currentWeatherCurrentCity.textContent = "";
+}
 // search for city, weather gets displayed to current-weather-info section in html //
 
 
@@ -17,8 +49,26 @@ fetch("https://api.github.com/users/octocat/repos").then(function(response) {
 
 // my api key: f95d4558f1f3deec5b9d71145b7381a7 //
 
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={f95d4558f1f3deec5b9d71145b7381a7}").then(function(response) {
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={process.env.API_KEY}").then(function(response) {
   response.json().then(function(data) {
       console.log(data);
   });
 });
+
+var getCurrentWeather = function(currentWeather) {
+    // openWeather API //
+    var apiKey = "process.env.API_KEY";
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?&appid={apiKey}";
+
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+// forecast display // 
+var forecastDayEl = document.querySelector("#card");
+forecastDayEl.textContent = ""
+  
